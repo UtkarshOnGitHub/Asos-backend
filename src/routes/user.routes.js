abcbe0d6b09f9853616e94304f07db528c612f8c
Utkarsh <famedustevershine@gmail.com>
2023-01-02 18:11:42 +0530

    .

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -54,48 +54,6 @@ user.post("/login" , async(req,res)=>{
 })
 
 
-// user.post("/forget" ,async(req,res)=>{
-//     const {email} = req.body
-//     const otp = Math.floor(Math.random()*1000000)
-//     let check = await otpModel.findOne({email});
-//     if(check){
-//         await otpModel.deleteOne({email});
-//     }
-//     const otpCollection = new otpModel({otp,email})
-//     await otpCollection.save()
-//     // transport.sendMail({
-//     //     to: email,
-//     //     from:"bloghead@gmail.com",
-//     //     subject:"OTP Sent Successfully ",
-//     //     text:`Hello ${email}, Your OTP is ${otp}`
-    
-//     // }).then((e)=>{
-//     //     console.log("Otp sent Successfully")
-//     //     res.send("Otp Sent Successfully")
-//     // })
-// })
-
-
-// user.post("/reset" , async (req,res)=>{
-//     const {email , newPass, otp} = req.body;
-//     console.log(email)
-//     let otps = await otpModel.findOne({email});
-//     console.log(otps)
-//     try{
-//         let checkOtp = otps.otp;
-//         if(checkOtp === otp){
-//             const delOtp = await otpModel.deleteOne({email});
-//             const user = await UserModel.findOneAndUpdate({email},{$set:{password:newPass}});
-//             res.status(200).send("Your Password Is Updated")
-//         }else{
-//             return res.send("invalid OTP")
-//         }
-//     }catch(e){
-//         return res.status(400).send("error")
-//     }
-// })
-
-
 user.post("/getuser" , async (req,res)=>{
     const {token} = req.body
     if(!token){
