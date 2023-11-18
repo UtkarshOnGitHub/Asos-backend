1c4b39a3a7d2a0869f4fe190c5e35c437448975c
Utkarsh <famedustevershine@gmail.com>
2023-11-18 12:14:55 +0530

    New Test

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const dbConnect = ()=>{
     mongoose.set('strictQuery', true);
-    return mongoose.connect('mongodb+srv://kira:kira@cluster0.6b3imib.mongodb.net/asosdatabase');
+    return mongoose.connect('mongodb+srv://kira:Zf5D24UEW25acrY0@cluster0.6b3imib.mongodb.net/asosdatabase');
 }
 module.exports = dbConnect;
